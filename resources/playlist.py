15f9ac1fb56e83b1ea68c29e7f8342a2b4e86d77
NirashaThennakoon <nmalinthi@gmail.com>
2024-03-25 10:51:43 +0200

    modified with pylint

--- resources/playlist.py
+++ resources/playlist.py
@@ -1,17 +1,45 @@
+"""
+   This module responsible for handling functions of playlist resources
+"""
+from jsonschema import validate, ValidationError, FormatChecker
+from werkzeug.exceptions import BadRequest
 from flask import jsonify, request, g
 from flask_restful import Resource
 from data_models.models import Playlist, PlaylistItem, Workout, Song
 from extensions import db
 from extensions import cache
-from jsonschema import validate, ValidationError, FormatChecker
-from werkzeug.exceptions import NotFound, Conflict, BadRequest, UnsupportedMediaType
 
 
 class PlaylistResource(Resource):
+    """
+    Represents a playlist of songs.
+    
+    Attributes:
+    - name (str): The name of the playlist.
+    - songs (list): A list of Song objects in the playlist.
+    """
     @cache.cached(timeout=60)
     def get(self, playlist):
+        """
+        Retrieve details of a playlist.
+
+        This method retrieves the details of a playlist including its ID, duration, 
+        and the list of songs it contains. The playlist and its associated songs
+        are retrieved from the database.
+
+        Parameters:
+        ----------
+        playlist : Playlist
+            The playlist object to retrieve details for.
+
+        Returns:
+        -------
+        Response
+            A JSON response containing the details of the playlist including its ID, 
+            duration, and a list of songs it contains.
+
+        """
         playlist_items = PlaylistItem.query.filter_by(playlist_id=playlist.playlist_id).all()
-               
         songs_list = []
         for item in playlist_items:
             song = Song.query.get(item.song_id)
@@ -34,12 +62,30 @@ class PlaylistResource(Resource):
             "songs_list": songs_list
             }
         return jsonify(playlist_dict)
-    
     # user can change the playlist song order
     def put(self, playlist):
+        """
+        Update a playlist.
+
+        This method updates the details of a playlist based on the provided JSON data.
+
+        Parameters:
+        ----------
+        playlist : Playlist
+            The playlist object to be updated.
+
+        Returns:
+        -------
+        tuple
+            A tuple containing an empty string and status code 204 if the update is successful.
+
+        Raises:
+        ------
+        BadRequest
+            If the provided JSON data fails validation or if there is an issue with the data format.
+        """
         if g.current_api_key.user.user_type != 'admin':
             return {"message": "Unauthorized access"}, 403
-        
         data = request.json
         if not data:
             return {"message": "No input data provided"}, 400
@@ -54,7 +100,7 @@ class PlaylistResource(Resource):
                 PlaylistItem.query.filter_by(playlist_id=playlist.playlist_id).delete()
 
                 # Re-enter the incoming song ids with the updated order
-                for index, song_id in enumerate(song_order):
+                for song_id in enumerate(song_order):
                     playlist_item = PlaylistItem(
                         playlist_id=playlist.playlist_id,
                         song_id=song_id,
@@ -64,12 +110,27 @@ class PlaylistResource(Resource):
             db.session.commit()
             cache.clear()
         except ValidationError as e:
-                raise BadRequest(description=str(e))
+            raise BadRequest(description=str(e)) from e
         except ValueError as e:
             return {"message": str(e)}, 400
         return "", 204
-    
     def delete(self, playlist):
+        """
+        Delete a playlist and its associated items.
+
+        This method deletes the specified playlist along with all its associated items.
+
+        Parameters:
+        ----------
+        playlist : Playlist
+            The playlist object to be deleted.
+
+        Returns:
+        -------
+        tuple
+            A tuple containing an empty string and status code 204 upon successful deletion.
+
+        """
         playlist_items = PlaylistItem.query.filter_by(playlist_id=playlist.playlist_id).all()
 
         # Delete playlist items
@@ -85,11 +146,31 @@ class PlaylistResource(Resource):
 
 
 class PlaylistCreation(Resource):
+    """
+    Resource for creating playlists based on workout IDs.
+
+    This class defines a resource for creating playlists based on workout IDs provided
+    in the request JSON data. It handles the creation of playlists with songs selected
+    according to the intensity of each workout.
+
+    """
     def post(self):
+        """
+        Create a playlist based on provided workout IDs.
+
+        This method creates a playlist based on the workout IDs provided in the request JSON data.
+        The playlist is created with songs selected according to the intensity of each workout.
+
+        Returns:
+        -------
+        tuple
+            A tuple containing a message indicating the success of playlist creation along with
+            the playlist ID and status code 201 upon successful creation.
+
+        """
         data = request.json
         if not data or 'workout_ids' not in data:
-            return {"message": "Invalid input data on CreatePlayList"}, 400            
-
+            return {"message": "Invalid input data on CreatePlayList"}, 400
         playlist_name_rec = data['playlist_name']
         workout_ids = data['workout_ids']
 
@@ -117,7 +198,6 @@ class PlaylistCreation(Resource):
                     genre = ["Metal", "Hardcore", "Dubstep"]
 
                 # Get songs based on workout duration and genre
-                
                 songs = Song.query.filter(Song.song_genre.in_(genre)).all()
                 temp_duration = 0.0
                 for song in songs:
@@ -128,11 +208,11 @@ class PlaylistCreation(Resource):
                     temp_duration += song.song_duration
                     if temp_duration >= duration:
                         break
-                
                 total_workouts_duration = total_workouts_duration + temp_duration
 
         # Create playlist
-        playlist = Playlist(playlist_duration=total_workouts_duration, playlist_name=playlist_name_rec)
+        playlist = Playlist(playlist_duration=total_workouts_duration,
+                            playlist_name=playlist_name_rec)
         db.session.add(playlist)
         db.session.commit()
 
@@ -146,4 +226,5 @@ class PlaylistCreation(Resource):
         db.session.commit()
         cache.clear()
 
-        return {"message": "Playlist created successfully", "playlist_id": playlist.playlist_id}, 201
+        return {"message": "Playlist created successfully", "playlist_id":
+                   playlist.playlist_id}, 201
